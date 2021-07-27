d0e266f912e62688be0b7fe7b67e7cd1173e1324
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-07-27 07:14:59 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ make
 make enum.x
 make polya.x
 make makestr.x
-make 2Dplot.x
+# make 2Dplot.x
 make convert_structures_to_enumformat.x
 make compare_enum_files.x
 make find_structure_in_list.x
@@ -16,7 +16,7 @@ mkdir -p ${PREFIX}/bin
 cp enum.x ${PREFIX}/bin/enum.x
 cp polya.x ${PREFIX}/bin/polya.x
 cp makestr.x ${PREFIX}/bin/makestr.x
-cp 2Dplot.x ${PREFIX}/bin/2Dplot.x
+# cp 2Dplot.x ${PREFIX}/bin/2Dplot.x
 cp convert_structures_to_enumformat.x ${PREFIX}/bin/convert_structures_to_enumformat.x
 cp compare_enum_files.x ${PREFIX}/bin/compare_enum_files.x
 cp find_structure_in_list.x ${PREFIX}/bin/find_structure_in_list.x
